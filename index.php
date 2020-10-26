6f11bf805a0a8c5c27d9cd07ce2e7e6a830d5787
matt4564 <72763755+matt4564@users.noreply.github.com>
2020-10-26 09:28:18 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<center><h1>Matthew's FTP server</h1></center>
+<center>You really <strong>should not</strong> be here</center>
+<div><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAd5JREFUeNqMU79rFUEQ/vbuodFEEkzAImBpkUabFP4ldpaJhZXYm/RiZWsv/hkWFglBUyTIgyAIIfgIRjHv3r39MePM7N3LcbxAFvZ2b2bn22/mm3XMjF+HL3YW7q28YSIw8mBKoBihhhgCsoORot9d3/ywg3YowMXwNde/PzGnk2vn6PitrT+/PGeNaecg4+qNY3D43vy16A5wDDd4Aqg/ngmrjl/GoN0U5V1QquHQG3q+TPDVhVwyBffcmQGJmSVfyZk7R3SngI4JKfwDJ2+05zIg8gbiereTZRHhJ5KCMOwDFLjhoBTn2g0ghagfKeIYJDPFyibJVBtTREwq60SpYvh5++PpwatHsxSm9QRLSQpEVSd7/TYJUb49TX7gztpjjEffnoVw66+Ytovs14Yp7HaKmUXeX9rKUoMoLNW3srqI5fWn8JejrVkK0QcrkFLOgS39yoKUQe292WJ1guUHG8K2o8K00oO1BTvXoW4yasclUTgZYJY9aFNfAThX5CZRmczAV52oAPoupHhWRIUUAOoyUIlYVaAa/VbLbyiZUiyFbjQFNwiZQSGl4IDy9sO5Wrty0QLKhdZPxmgGcDo8ejn+c/6eiK9poz15Kw7Dr/vN/z6W7q++091/AQYA5mZ8GYJ9K0AAAAAASUVORK5CYII=" alt="Italian Trulli"><a href="https://doc-14-a0-docs.googleusercontent.com/docs/securesc/3980is6iku0nc7mn54b5la0k1gqckof6/l6cfulrottulmdfsb566pte7lvvsh97m/1603718625000/00918488156978824116/00918488156978824116/1ODTpQ_AmafDqWTZ9Qx6npp8GfKCKDMQw?e=download&authuser=0&nonce=j3qequak8aq5a&user=00918488156978824116&hash=pqvnf3egjgrb8mi6g832pe2lqqh1k0u5">GoGuardian Source code</a></div>
+</body>
+</html>
